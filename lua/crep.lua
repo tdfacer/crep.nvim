daadda1e716e0defd8fe7659b51413b1a4697664
Trevor Facer <trevorf@ifit.com>
2022-05-29 13:15:27 -0600

    chore(crep.lua): Add plugin setup and repo list
    This adds some basic functionality:
    * Pass in organization via setup options
    * Query repo list using plenary job

--- lua/crep.lua
+++ lua/crep.lua
@@ -1,64 +1,83 @@
 local Job = require 'plenary.job'
-local Job = require 'plenary.job'
-
 
 -- name,description,pushedAt
 -- use this to get list of json fields:
 -- gh repo list --json 2>&1 | v -
 
-Job:new({
-  command = 'gh',
-  args = { 'repo', 'list', '--json', 'name,description,pushedAt' },
-  -- cwd = '/home/trevor/code/opsgenie-ts',
-  -- cwd = '/usr/bin',
-  -- env = { ['a'] = 'b' },
-  on_exit = function(j, return_val)
-    -- print(return_val)
-    -- print(type(return_val))
-    -- if return_val > 0 or return_val ~= "0" then
-    --   print("failed to complete command with error code: " .. return_val)
-    -- end
-    if return_val > 0 then
-      print("failed to complete command with error code: " .. return_val)
-      -- print("failed to complete command with error code: " .. return_val .. "; " .. j)
-      -- print("err: " .. j)
-    else
-      print(vim.inspect(j:result()))
-    end
+local _M = {}
 
-    local result = j:result()
+_M.setup = function(opts)
+  _M.organization = opts.organization or ""
+end
 
-    local ok, parsed = pcall(vim.json.decode, table.concat(result, ""))
+_M.do_stuff = function()
+  print("doing stuff!")
+end
 
-    if not ok then
-      print("was not ok. ok: " .. ok)
-    else
-      -- print("ok! ok: " .. ok)
-      string.format("ok!: ok: %s", ok)
-    end
+_M.get_repos = function()
+  Job:new({
+    command = 'gh',
+    -- args = { 'repo', 'list', _M.organization, '--json', 'name,description,pushedAt' },
+    args = { 'repo', 'list', _M.organization, '--json', 'name,description,pushedAt' },
+    -- cwd = '/home/trevor/code/opsgenie-ts',
+    -- cwd = '/usr/bin',
+    -- env = { ['a'] = 'b' },
+    on_exit = function(j, return_val)
+      if return_val > 0 then
+        print("failed to complete command with error code: " .. return_val)
+        return {}
+      else
+        print(vim.inspect(j:result()))
+      end
 
-    if not parsed then
-      print("was not parsed. parsed: " .. parsed)
-    else
-      -- print("parsed! parsed: " .. parsed)
-      string.format("parsed! parsed: %parsed", parsed)
-    end
+      local result = j:result()
 
-    for k, v in pairs(parsed) do
-      print(string.format("parsed key: %s, val: %s", k, vim.inspect(v)))
-    end
+      local ok, parsed = pcall(vim.json.decode, table.concat(result, ""))
 
+      if not ok then
+        print("was not ok. ok: " .. ok)
+      else
+        -- print("ok! ok: " .. ok)
+        string.format("ok!: ok: %s", ok)
+      end
 
-    -- for k, v in ipairs(j:result()) do
-    --   print(k)
-    -- end
+      if not parsed then
+        print("was not parsed. parsed: " .. parsed)
+      else
+        -- print("parsed! parsed: " .. parsed)
+        string.format("parsed! parsed: %parsed", parsed)
+      end
 
-    -- print(vim.inspect(return_val))
-    -- print(vim.inspect(j:result()))
-  end,
-  -- on_error = function(e, res)
-  --   print("error: " .. e)
-  --   print("res: " .. res)
-  -- end,
-  -- }):sync() -- or start()
-}):start() -- or start()
+      for k, v in pairs(parsed) do
+        -- print(string.format("parsed key: %s, val: %s", k, vim.inspect(v)))
+        print("name: " .. v.name)
+        print("description: " .. v.description)
+        print("pushedAt: " .. v.pushedAt)
+      end
+
+      -- local newbuf = vim.api.nvim_create_buf(false, true)
+
+      -- vim.api.nvim_open_term()
+
+      -- for k, v in ipairs(j:result()) do
+      --   print(k)
+      -- end
+
+      -- print(vim.inspect(return_val))
+      -- print(vim.inspect(j:result()))
+    end,
+    -- on_error = function(e, res)
+    --   print("error: " .. e)
+    --   print("res: " .. res)
+    -- end,
+    -- }):sync() -- or start()
+  }):start() -- or start()
+end
+
+_M.setup({
+  organization = "ifit",
+})
+_M.do_stuff()
+_M.get_repos()
+
+return _M
