08a4026788aa71eef13155d641552241ac96f927
Trevor Facer <trevorf@ifit.com>
2022-05-30 07:38:37 -0600

    chore(comments): Clean up some comments

--- lua/telescope/_extensions/crep.lua
+++ lua/telescope/_extensions/crep.lua
@@ -9,30 +9,3 @@ return require 'telescope'.register_extension {
     get_repos = crep.get_repos,
   },
 }
--- return require('telescope').register_extension {
---   setup = function(ext_config)
---     filetypes = ext_config.filetypes or {"png", "jpg", "gif", "mp4", "webm", "pdf"}
---     find_cmd = ext_config.find_cmd or "fd"
---   end,
---   exports = {
---     media_files = M.media_files
---   },
--- }
---
--- telescope.setup {
---   extensions = {
---     frecency = {
---       db_root = "home/my_username/path/to/db_root",
---       show_scores = false,
---       show_unindexed = true,
---       ignore_patterns = {"*.git/*", "*/tmp/*"},
---       disable_devicons = false,
---       workspaces = {
---         ["conf"]    = "/home/my_username/.config",
---         ["data"]    = "/home/my_username/.local/share",
---         ["project"] = "/home/my_username/projects",
---         ["wiki"]    = "/home/my_username/wiki"
---       }
---     }
---   },
--- }
