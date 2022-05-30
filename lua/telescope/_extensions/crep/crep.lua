d14971ddd0be53370ccb51cad28f84cd6bd06ed0
Trevor Facer <trevorf@ifit.com>
2022-05-30 09:08:44 -0600

    chore(crep): Almost working with cache

--- lua/telescope/_extensions/crep/crep.lua
+++ lua/telescope/_extensions/crep/crep.lua
@@ -12,169 +12,17 @@ local pickers = require 'telescope.pickers'
 local previewers = require 'telescope.previewers.term_previewer'
 local utils = require 'telescope.utils'
 
-local temp_repos = {
-  {
-    description = "Quick start repository for creating a Terraform provider",
-    name = "terraform-provider-scaffolding",
-    pushedAt = "2022-05-24T07:00:36Z"
-  },
-  {
-    description = "",
-    name = "tdfacer.github.io",
-    pushedAt = "2022-05-23T00:13:44Z"
-  },
-  {
-    description = "",
-    name = "ifit-static-test",
-    pushedAt = "2022-04-16T20:11:41Z"
-  },
-  {
-    description = "",
-    name = "cloudops",
-    pushedAt = "2022-04-15T23:16:14Z"
-  },
-  {
-    description = "Save Our Summer (SOS): A family bucket list application",
-    name = "sos",
-    pushedAt = "2022-04-09T03:16:05Z"
-  },
-  {
-    description = "Terraform and Cloudformaton IaC",
-    name = "terrafacer",
-    pushedAt = "2022-03-14T15:30:25Z"
-  },
-  {
-    description = "",
-    name = "family-goals-frontend",
-    pushedAt = "2022-03-03T01:02:31Z"
-  },
-  {
-    description = "",
-    name = "spot-checker",
-    pushedAt = "2022-01-27T01:21:22Z"
-  },
-  {
-    description = "",
-    name = "brightcove-deprecation",
-    pushedAt = "2022-01-14T23:11:37Z"
-  },
-  {
-    description = "",
-    name = "family-goals",
-    pushedAt = "2022-01-01T18:32:53Z"
-  },
-  {
-    description = "",
-    name = "family-goals-service",
-    pushedAt = "2022-01-01T18:20:07Z"
-  },
-  {
-    description = "hello world for graphql with typescript",
-    name = "typescript-graphql",
-    pushedAt = "2021-12-26T23:07:51Z"
-  },
-  {
-    description = "",
-    name = "opsgenie-alarms-2",
-    pushedAt = "2021-12-23T16:46:57Z"
-  },
-  {
-    description = "",
-    name = "maizey_math",
-    pushedAt = "2021-10-24T14:29:26Z"
-  },
-  {
-    description = "",
-    name = "2021_09_07_incident_response",
-    pushedAt = "2021-09-21T22:53:31Z"
-  },
-  {
-    description = "PKGBUILD for maintaining Aliyun CLI package in AUR",
-    name = "aliyun-cli-bin",
-    pushedAt = "2021-05-14T14:03:53Z"
-  },
-  {
-    description = " =house_with_garden: Open source home automation that puts local control and privacy first",
-    name = "core",
-    pushedAt = "2021-03-19T17:25:19Z"
-  },
-  {
-    description = "",
-    name = "consciousness",
-    pushedAt = "2021-03-14T18:48:18Z"
-  },
-  {
-    description = "",
-    name = "mqttjs",
-    pushedAt = "2021-03-04T15:27:25Z"
-  },
-  {
-    description = "",
-    name = "mongo-terraform",
-    pushedAt = "2021-01-23T17:04:07Z"
-  },
-  {
-    description = "",
-    name = "schedules",
-    pushedAt = "2021-01-18T03:14:55Z"
-  },
-  {
-    description = "",
-    name = "devops",
-    pushedAt = "2020-12-05T16:51:09Z"
-  },
-  {
-    description = "",
-    name = "covid-care-kits",
-    pushedAt = "2020-10-31T16:45:27Z"
-  },
-  {
-    description = "",
-    name = "brightcove-tests",
-    pushedAt = "2020-10-29T05:00:35Z"
-  },
-  {
-    description = "Command-line program to download videos from YouTube.com and other video sites ",
-    name = "youtube-dl",
-    pushedAt = "2020-10-25T11:29:05Z"
-  },
-  {
-    description = "",
-    name = "xmlbuilder",
-    pushedAt = "2020-09-09T04:56:35Z"
-  },
-  {
-    description = "",
-    name = "sandbox-elemental",
-    pushedAt = "2020-07-27T21:18:10Z"
-  },
-  {
-    description = "",
-    name = "sos-static",
-    pushedAt = "2020-07-27T13:39:41Z"
-  },
-  {
-    description = "",
-    name = "s3-handler",
-    pushedAt = "2020-04-23T23:02:06Z"
-  },
-  {
-    description = "",
-    name = "nginx-wolf-updates",
-    pushedAt = "2019-12-19T19:39:48Z"
-  }
-}
-
 -- name,description,pushedAt
 -- use this to get list of json fields:
 -- gh repo list --json 2>&1 | v -
 
 local _M = {}
 
-_M.state = {
-  organization = "",
-  destination_dir = "/tmp"
-}
+-- _M.state = {
+--   organization = "",
+--   destination_dir = "/tmp",
+--   results = {},
+-- }
 
 _M.clone_repo = function(opts)
   print(string.format("cloning %s/%s", opts.organization, opts.repo))
@@ -188,17 +36,19 @@ _M.clone_repo = function(opts)
         return {}
       else
         print("successfully cloned: " .. opts.repo)
-        vim.cmd('cd ' .. _M.destination_dir .. "/" .. opts.repo)
+        -- vim.cmd('cd ' .. _M.destination_dir .. "/" .. opts.repo)
         return {}
       end
     end,
-  }):start() -- or start()
+  }):sync() -- or start()
 end
 
 _M.setup = function(opts)
   vim.pretty_print(opts)
   _M.organization = opts.organization and opts.organization or ""
   _M.destination_dir = opts.destination_dir and opts.destination_dir or "/tmp/"
+  _M.temp_file = "/tmp/repo_list.json"
+  return _M
 end
 
 local function gen_from_gh_repo_list(opts)
@@ -233,9 +83,10 @@ local function gen_from_gh_repo_list(opts)
   end
 end
 
-function print_info()
+local function print_info()
   print(string.format("organization: %s", _M.organization))
   print(string.format("destination_dir: %s", _M.destination_dir))
+  print(string.format("temp_file: %s", _M.temp_file))
 end
 
 _M.get_repos = function(opts)
@@ -254,44 +105,63 @@ _M.get_repos = function(opts)
   -- vim.pretty_print(check)
 
   local all_results = {}
-  Job:new({
-    command = 'gh',
-    args = { 'repo', 'list', _M.organization, '--json', 'name,description,pushedAt' },
-    on_exit = function(j, return_val)
-      if return_val > 0 then
-        print("failed to complete command with error code: " .. return_val)
-        return {}
-        -- else
-        --   print(vim.inspect(j:result()))
-      end
 
-      local result = j:result()
+  print_info()
+  -- local res_path = Path:new { _M.temp_file }
+  local res_path = Path:new { "/tmp/repo_list.json" }
+  if not res_path:exists() then
+    print("temp file does not exist yet at " .. _M.temp_file)
+  else
+    local fh = io.open("/tmp/repo_list.json")
+    if fh ~= nil then
+      all_results = vim.json.decode(fh:read("*a"))
+    end
+  end
 
-      local ok, results = pcall(vim.json.decode, table.concat(result, ""))
+  if #all_results <= 0 then
+    Job:new({
+      command = 'gh',
+      args = { 'repo', 'list', _M.organization, '--json', 'name,description,pushedAt' },
+      -- timeout = 30000,
+      on_exit = function(j, return_val)
+        if return_val > 0 then
+          print("failed to complete command with error code: " .. return_val)
+          return {}
+        end
 
-      if not ok then
-        print("was not ok. ok: " .. ok)
-      else
-        -- print("ok! ok: " .. ok)
-        string.format("ok!: ok: %s", ok)
-      end
+        local result = j:result()
 
-      if not results then
-        print("was not parsed. parsed: " .. results)
-      else
-        -- print("parsed! parsed: " .. parsed)
-        string.format("parsed! parsed: %parsed", results)
-      end
+        local ok, results = pcall(vim.json.decode, table.concat(result, ""))
+        -- vim.json.encode
 
-      for _, v in pairs(results) do
-        -- print(string.format("parsed key: %s, val: %s", k, vim.inspect(v)))
-        -- print("name: " .. v.name)
-        -- print("description: " .. v.description)
-        -- print("pushedAt: " .. v.pushedAt)
-        table.insert(all_results, v)
-      end
-    end,
-  }):sync() -- or start()
+        if not ok then
+          print("was not ok. ok: " .. ok)
+        else
+          string.format("ok!: ok: %s", ok)
+        end
+
+        if not results then
+          print("was not parsed. parsed: " .. results)
+        else
+          string.format("parsed! parsed: %parsed", results)
+        end
+
+        -- save results to a json file
+        -- local fh = io.open(_M.temp_file, "w")
+        local fh = io.open("/tmp/repo_list.json", "w")
+        if fh ~= nil then
+          fh:write(vim.json.encode(result))
+          fh:close()
+        end
+        -- temp_file
+
+        for _, v in pairs(results) do
+          table.insert(all_results, v)
+        end
+      end,
+    }):sync(5000) -- or start()
+    -- }):sync(60000) -- or start()
+  end
   -- }):start() -- or start()
 
   pickers.new(opts, {
@@ -316,8 +186,8 @@ _M.get_repos = function(opts)
           _M.clone_repo({ organization = _M.organization, repo = entry.name })
         else
           print(entry.name .. " exists")
-          vim.cmd('cd ' .. _M.destination_dir .. "/" .. entry.name)
         end
+        vim.cmd('cd ' .. _M.destination_dir .. "/" .. entry.name)
         -- if not path:exists() then return nil end
         --       local exists = path:exists()
         -- local dir = from_entry.path(entry)
